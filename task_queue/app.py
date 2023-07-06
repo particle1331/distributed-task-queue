67a1b656903714de8430a2c66979185a9ab572f0
particle1331 <54845098+particle1331@users.noreply.github.com>
2023-07-06 15:09:41 +0800

    Simplify main branch

--- task_queue/app.py
+++ task_queue/app.py
@@ -1,8 +1,6 @@
 import os
 
 from celery import Celery
-from celery.schedules import crontab
-from kombu import Exchange, Queue
 
 REDIS_HOST = os.environ["REDIS_HOST"]
 REDIS_PORT = os.environ["REDIS_PORT"]
@@ -13,37 +11,22 @@ PROJECT_NAME = "distributed-task-queue"
 BACKEND_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}/0"
 BROKER_URL = f"pyamqp://guest:guest@{RABBITMQ_HOST}:{RABBITMQ_PORT}//"
 
+app = Celery(
+    PROJECT_NAME,
+    broker=BROKER_URL,
+    backend=BACKEND_URL,
+    include=("task_queue.tasks",),
+)
+
+
 CELERY_CONFIG = {
     "task_acks_late": True,
     "worker_prefetch_multiplier": 1,
     "task_default_priority": 0,
-    "task_queue_max_priority": 5,
+    "task_queue_max_priority": 10,
     "task_create_missing_queues": False,
     "result_expires": None,
     "result_extended": True,
 }
 
-app = Celery(PROJECT_NAME, broker=BROKER_URL, backend=BACKEND_URL)
 app.conf.update(CELERY_CONFIG)
-app.conf.task_queues = [
-    Queue(
-        'celery',
-        Exchange('celery'),
-        routing_key='celery',
-        queue_arguments={'x-max-priority': 10},
-    ),
-]
-
-app.conf.beat_schedule = {
-    'random-fail-every-10-seconds': {
-        'task': 'task_queue.tasks.random_fail',
-        'schedule': 10,
-        'args': (),
-    },
-    'sleep-every-1-minute': {
-        'task': 'task_queue.tasks.sleep',
-        'schedule': crontab(),
-        'args': (1, 0),
-        'options': {'priority': 3},
-    },
-}
