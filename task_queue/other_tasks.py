664cc9997552ac95dec5f85f65fc6dd5ef349c9d
particle1331 <54845098+particle1331@users.noreply.github.com>
2023-07-07 05:49:48 +0800

    Add retries

--- task_queue/other_tasks.py
+++ task_queue/other_tasks.py
@@ -4,7 +4,12 @@ import time
 from task_queue.app import app
 
 
-@app.task
+@app.task(
+    autoretry_for=(ZeroDivisionError,),
+    max_retries=2,
+    retry_backoff=3,
+    retry_jitter=True
+)
 def random_fail():
     time.sleep(0.1)
     x = random.choice([0, 1])
