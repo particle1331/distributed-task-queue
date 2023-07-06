0813ecd05078775caa04a5e6f42b17f7a181656b
particle1331 <54845098+particle1331@users.noreply.github.com>
2023-07-06 21:15:03 +0800

    Further celery features

--- task_queue/app.py
+++ task_queue/app.py
@@ -1,6 +1,8 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
+from kombu import Exchange, Queue
 
 REDIS_HOST = os.environ["REDIS_HOST"]
 REDIS_PORT = os.environ["REDIS_PORT"]
@@ -15,9 +17,18 @@ app = Celery(
     PROJECT_NAME,
     broker=BROKER_URL,
     backend=BACKEND_URL,
-    include=("task_queue.tasks",),
+    include=(
+        "task_queue.tasks",
+        "task_queue.signals",
+    ),
 )
 
+
+OTHER_NAME = "other"
+DEFAULT_NAME = "default"
+exchange = Exchange(DEFAULT_NAME, type="direct")
+other_queue = Queue(OTHER_NAME, exchange, routing_key=OTHER_NAME)
+default_queue = Queue(DEFAULT_NAME, exchange, routing_key=DEFAULT_NAME)
 
 CELERY_CONFIG = {
     "task_acks_late": True,
@@ -27,6 +38,25 @@ CELERY_CONFIG = {
     "task_create_missing_queues": False,
     "result_expires": None,
     "result_extended": True,
+    "task_queues": (default_queue, other_queue),
+    "task_default_queue": DEFAULT_NAME,
+    "task_default_exchange": DEFAULT_NAME,
+    "task_default_routing_key": DEFAULT_NAME,
 }
 
 app.conf.update(CELERY_CONFIG)
+
+
+app.conf.beat_schedule = {
+    "random-fail-every-10-seconds": {
+        "task": "task_queue.tasks.random_fail",
+        "schedule": 10,
+        "args": (),
+    },
+    "sleep-every-1-minute": {
+        "task": "task_queue.tasks.sleep",
+        "schedule": crontab(),
+        "args": (1, 0),
+        "options": {"priority": 3},
+    },
+}
