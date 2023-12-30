cf14e36ce0ba2d884c7dd3404be41381321643bf
particle1331 <54845098+particle1331@users.noreply.github.com>
2023-12-31 07:28:42 +0800

    Update revoke messages

--- api/tasks.py
+++ api/tasks.py
@@ -64,7 +64,7 @@ async def revoke_task(task_id: str) -> JSONResponse:
         status_code = status.HTTP_400_BAD_REQUEST
     else:
         celery_app.control.revoke(task_id)
-        message = f"Task {task_id} revoked successfully."
+        message = f"Task {task_id} revoked."
         status_code = status.HTTP_200_OK
 
     return JSONResponse({"message": message}, status_code=status_code)
@@ -77,7 +77,7 @@ async def abort_task(task_id: str) -> JSONResponse:
         status_code = status.HTTP_400_BAD_REQUEST
     else:
         celery_app.control.revoke(task_id, terminate=True)
-        message = f"Task {task_id} aborted successfully."
+        message = f"Task {task_id} aborted."
         status_code = status.HTTP_200_OK
 
     return JSONResponse({"message": message}, status_code=status_code)
