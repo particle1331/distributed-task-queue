dcf6fda3ad38c2e6fa6dd8c4bb1849c05ff947ff
particle1331 <54845098+particle1331@users.noreply.github.com>
2023-12-31 16:41:38 +0800

    Simplify queues; Add other worker to compose

--- dtq/app.py
+++ dtq/app.py
@@ -1,8 +1,7 @@
 import os
-
 from celery import Celery
 from celery.schedules import crontab
-from kombu import Exchange, Queue
+
 
 REDIS_HOST = os.environ["REDIS_HOST"]
 REDIS_PORT = os.environ["REDIS_PORT"]
@@ -12,6 +11,7 @@ RABBITMQ_PORT = os.environ["RABBITMQ_PORT"]
 PROJECT_NAME = "distributed-task-queue"
 BACKEND_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}/0"
 BROKER_URL = f"pyamqp://guest:guest@{RABBITMQ_HOST}:{RABBITMQ_PORT}//"
+
 
 app = Celery(
     PROJECT_NAME,
@@ -24,37 +24,21 @@ app = Celery(
     ),
 )
 
-
 CELERY_CONFIG = {
     "task_acks_late": True,
     "worker_prefetch_multiplier": 1,
     "task_default_priority": 0,
     "task_queue_max_priority": 10,
-    "task_create_missing_queues": False,
+    "task_create_missing_queues": True,
     "result_expires": None,
     "result_extended": True,
 }
 
-# Default name celery -> backward compatible
-OTHER_NAME = "other"
-DEFAULT_NAME = "celery"
-exchange = Exchange(DEFAULT_NAME, type="direct")
-other_queue = Queue(OTHER_NAME, exchange, routing_key=OTHER_NAME)
-default_queue = Queue(DEFAULT_NAME, exchange, routing_key=DEFAULT_NAME)
-
-ROUTING_CONFIG = {
-    "task_queues": (default_queue, other_queue),
-    "task_default_queue": DEFAULT_NAME,
-    "task_default_exchange": DEFAULT_NAME,
-    "task_default_routing_key": DEFAULT_NAME,
-    "task_routes": {
-        "dtq.other_tasks.*": {"queue": "other"},
-    },
-}
-
 app.conf.update(CELERY_CONFIG)
-app.conf.update(ROUTING_CONFIG)
-
+app.conf.task_routes = {
+    'dtq.tasks.*': {'queue': 'celery'},
+    'dtq.other_tasks.*': {'queue': 'other'}
+}
 
 app.conf.beat_schedule = {
     "random-fail-every-10-seconds": {
